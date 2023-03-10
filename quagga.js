de8b8f907e340e705fb0a2532e61f73b65552bbd
EXECUTIVEGS-6 <EXECUTIVEGS-6@fivesdigital.com>
2023-03-10 16:17:45 +0530

    added

---
+++ quagga.js
@@ -0,0 +1,36 @@
+// Load QuaggaJS
+Quagga.init({
+    inputStream: {
+      // Use the camera stream as the input source
+      constraints: {
+        facingMode: 'environment' // use the back camera on mobile devices
+      },
+      area: { // defines rectangle of the detection area in relation to the canvas element
+        top: "30%",    // top offset
+        right: "15%",  // right offset
+        left: "15%",   // left offset
+        bottom: "30%"  // bottom offset
+      }
+    },
+    decoder: {
+      // Define which types of codes to scan
+      readers: ['ean_reader']
+    }
+  }, function(err) {
+    if (err) {
+      console.error('Error initializing Quagga', err);
+      return;
+    }
+    console.log('Quagga initialized');
+    
+    // Start the scanner when the video stream is ready
+    video.addEventListener('canplay', function() {
+      Quagga.start();
+    });
+    
+    // Listen for barcode scans
+    Quagga.onDetected(function(result) {
+      console.log('Barcode detected', result.codeResult.code);
+    });
+  });
+  
---
+++ scanner.js
@@ -0,0 +1,11 @@
+const video = document.getElementById('video');
+
+// Use navigator.mediaDevices.getUserMedia to get access to the camera
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then((stream) => {
+    // Set the video source to the camera stream
+    video.srcObject = stream;
+  })
+  .catch((error) => {
+    console.error('Error accessing camera', error);
+  });
